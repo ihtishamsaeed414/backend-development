434700d7cf4c2f9e4a3b3e4427d67f64062d3cc8
ihtisham saeed <ihtishamsaeed7780@gmail.com>
2024-10-29 12:52:10 +0500

    Added node, .env

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
-// setting up the server
-import http from "http"; // mkaing an http server
+import http from "http";
 
-const PORT = 8000;
+const PORT = process.env.PORT;
 
 const server = http.createServer((req, res) => {
-  // res.write("Hello World");
-  // res.setHeader("Content-Type", "text/html"); //sending header to the server
-  // res.statusCode = 404; // witht the status code of 404
-  res.writeHead(500, { "Content-Type": "application/json" });
-  res.end(JSON.stringify({ message: "Server Error" }));
-  // res.end("<h1> Hello World </>"); //dont need to write first
+  res.writeHead(500, { "Content-Type": "text/html" });
+  res.end("<h1> Hello World! </>");
 });
 
 server.listen(PORT, () => {

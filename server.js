849cbc787c61f6f0322ad1485d1602554d905054
ihtisham saeed <ihtishamsaeed7780@gmail.com>
2024-10-29 15:40:19 +0500

    basic api request

--- server.js
+++ server.js
@@ -3,8 +3,20 @@ import http from "http";
 const PORT = process.env.PORT;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(500, { "Content-Type": "text/html" });
-  res.end("<h1> Hello World! </>");
+  try {
+    if (req.url === "/") {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end("<h1> I am Home </>");
+    } else if (req.url === "/about") {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end("<h1> I am about page </>");
+    } else if (req.url === "/contact") {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end("<h1> I am contact page </>");
+    }
+  } catch (error) {
+    throw new Error("internal server error");
+  }
 });
 
 server.listen(PORT, () => {

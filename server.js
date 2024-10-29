fce822acb5cd78e4ba60abb324a4af16baa32f98
ihtisham saeed <ihtishamsaeed7780@gmail.com>
2024-10-29 10:42:16 +0500

    created a server and created a start script package.json file

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 // setting up the server
-import http from "http";
+import http from "http"; // mkaing an http server
 
-const PORT = 4000;
+const PORT = 8000;
 
 const server = http.createServer((req, res) => {
-  res.write("Hello World");
-  res.end();
+  // res.write("Hello World");
+  // res.setHeader("Content-Type", "text/html"); //sending header to the server
+  // res.statusCode = 404; // witht the status code of 404
+  res.writeHead(500, { "Content-Type": "application/json" });
+  res.end(JSON.stringify({ message: "Server Error" }));
+  // res.end("<h1> Hello World </>"); //dont need to write first
 });
 
 server.listen(PORT, () => {

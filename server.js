a6c7215672fa6078850aec1a66c96e76f716166c
ihtisham saeed <ihtishamsaeed7780@gmail.com>
2024-10-29 16:12:08 +0500

    Checking for

--- server.js
+++ server.js
@@ -4,18 +4,23 @@ const PORT = process.env.PORT;
 
 const server = http.createServer((req, res) => {
   try {
-    if (req.url === "/") {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.end("<h1> I am Home </>");
-    } else if (req.url === "/about") {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.end("<h1> I am about page </>");
-    } else if (req.url === "/contact") {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.end("<h1> I am contact page </>");
+    if (req.method === "GET") {
+      if (req.url === "/") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end("<h1> I am Home </>");
+      } else if (req.url === "/about") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end("<h1> I am about page </>");
+      } else if (req.url === "/contact") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end("<h1> I am contact page </>");
+      }
+    } else {
+      throw new Error("Method not allowed");
     }
   } catch (error) {
-    throw new Error("internal server error");
+    res.writeHead(500, { "Content-Type": "text" });
+    res.end("server error"); // this is basically res.end on the browser page so it will not error it will only accept string
   }
 });
 

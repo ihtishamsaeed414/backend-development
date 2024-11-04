6da07969b184b89084078149bb63c8404df75f09
ihtisham saeed <ihtishamsaeed7780@gmail.com>
2024-11-04 17:08:00 +0500

    users dynamic fetch

--- server2.js
+++ server2.js
@@ -12,9 +12,20 @@ const server = createServer((req, res) => {
     res.write(JSON.stringify(users));
     res.end();
   } else if (req.url.match(/\/api\/users\/([0-9]+)/) && req.method === "GET") {
-    res.setHeader("Content-Type", "application/json");
-    res.write(JSON.stringify({ id: 4, name: "Ihtisham Saeed" }));
-    res.end();
+    const id = req.url.split("/")[3];
+    const user = users.find((user) => {
+      return user.id === parseInt(id); //It is important
+    });
+    if (user) {
+      res.setHeader("Content-Type", "application/json");
+      res.write(JSON.stringify(user));
+      res.end();
+    } else {
+      res.setHeader("Content-Type", "application/json");
+      res.statusCode = 404;
+      res.write(JSON.stringify({ message: "User not Found" }));
+      res.end();
+    }
   } else {
     res.setHeader("Content-Type", "application/json");
     res.statusCode = 404;

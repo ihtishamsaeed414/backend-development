214e6f9b3b14241fef8bfdded1f17eb2cf49110f
ihtisham saeed <ihtishamsaeed7780@gmail.com>
2024-11-05 14:48:38 +0500

    fs modules with promises

--- fsModules.js
+++ fsModules.js
@@ -1,12 +1,28 @@
-import fs from "fs";
+// import fs from "fs";
+import fs from "fs/promises";
 
-// readFile asyncrhonous  - callback
-fs.readFile("./orders.txt", "utf8", (err, data) => {
-  if (err) throw err;
-  console.log(data);
-});
+// readFile asyncrhonous - callback
+// fs.readFile("./orders.txt", "utf8", (err, data) => {
+//   if (err) throw err;
+//   console.log(data);
+// });
 
 // readFileSync - syncrhonous verison
 
-const data = fs.readFileSync("./orders.txt", "utf8");
-console.log(data);
+// const data = fs.readFileSync("./orders.txt", "utf8");
+// console.log(data);
+
+// fs modules using promises
+fs.readFile("./orders.txt", "utf8")
+  .then((data) => console.log(data))
+  .catch((err) => console(err));
+
+//   fs using async await
+const readFile = async () => {
+  try {
+    const data = await fs.readFile("./orders.txt", "utf8");
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
